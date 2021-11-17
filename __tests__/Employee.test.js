8cb22bc7cc1e1aa55bd557bff9fad2aca45c3549
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-16 18:58:45 -0700

    tests for eomployee, 1 failed test for getrole()

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,34 @@
-const { jest } = require("jest");
+const Employee = require('../lib/Employee.js');
+
 
 test('creates an employee object', () => {
-    const employee = new Employee('manager', 'other')
+    const employee = new Employee('Bridget', 23, 'bridget@gmail.com');
 
-    expect(employee.name).toBe('manager');
-    expect(employee.status).toBe('employed');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.name).toEqual(expect.any(String));
+});
+
+
+test('check the employee name comes back from getName', () => {
+    const employeeName = new Employee('Bridget');
+    expect(employeeName.getName()).toEqual(expect.any(String));
+});
+
+test('check that getID returns a number', () => {
+    const idNumber = new Employee('Bridget', 23, 'bridget@gmail.com');
+    expect(idNumber.getId()).toEqual(expect.any(Number));
+});
+
+test('check that a string is returned for email', () => {
+    const email = new Employee('Bridget', 23, 'bridget@gmail.com');
+    expect(email.getEmail()).toEqual(expect.any(String));
+});
+
+test('make sure employee role is returned as employee', () => {
+    const empRole = new Employee('Bridget', 23, 'bridget@gmail.com');
+    //why doesnt this work 
+    // expect(empRole.getRole()).toEqual(stringContaining('Employee'));
+    // expect(empRole.getRole()).toEqual('Employee');
+    
 });
